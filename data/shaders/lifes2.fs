a9115be93ac523814891fd9b2123cb6d8895914f
OscarColom <oscar.colom01@estudiant.upf.edu>
2024-06-11 22:19:47 +0200

    retoques

--- data/shaders/lifes2.fs
+++ data/shaders/lifes2.fs
@@ -9,6 +9,9 @@ void main()
 	vec4 my_color = u_color;
 	float n_lifes = float(lifes) / 5.0;
 
+	my_color.rgb *= 0.8;
+
+
 	// Si las vidas son una, hacemos que la barra parpadee
 	if (lifes == 1) {
 		float blink = abs(sin(u_time * 5.0)); // Parpadeo basado en el tiempo
@@ -17,6 +20,9 @@ void main()
 
 	if (v_uv.x > n_lifes)
 		my_color *= 0.2; // Mantener la parte vacía de la barra más oscura
+
+	float gradient = smoothstep(0.4, 0.6, v_uv.y); // Crear un gradiente suave en el medio del eje Y
+    my_color.rgb *= mix(1.0, 1.2, gradient); // Incrementar el brillo en el medio del eje Y
 
 	gl_FragColor = my_color;
 }
--- data/shaders/stamina.fs
+++ data/shaders/stamina.fs
@@ -9,6 +9,9 @@ void main()
     vec4 my_color = u_color;
     float n_stamina = stamina / 50.0;
 
+    // Reducir la intensidad del color base
+    my_color.rgb *= 0.8;
+
     // Si la estamina es menor que 5, hacemos que la barra parpadee
     if (stamina < 5.0) {
         float blink = abs(sin(u_time * 5.0)); // Parpadeo basado en el tiempo
@@ -17,6 +20,10 @@ void main()
 
     if (v_uv.x > n_stamina)
         my_color *= 0.2; // Mantener la parte vacía de la barra más oscura
+
+    // Añadir un efecto de brillo a lo largo del eje Y siempre
+    float gradient = smoothstep(0.4, 0.6, v_uv.y); // Crear un gradiente suave en el medio del eje Y
+    my_color.rgb *= mix(1.0, 1.2, gradient); // Incrementar el brillo en el medio del eje Y
 
     gl_FragColor = my_color;
 }
